d81ae99f2fbc740e020b92fc403831ed1188ecd5
lossycrypt <lossycrypt@users.noreply.github.com>
2020-09-01 00:59:25 +0200

    updated cutom-input consuming description to reflect 3 year old changes :p

--- data.lua
+++ data.lua
@@ -90,12 +90,6 @@ data:extend({
     -- For reference these are the possible values for "consuming":
 
     -- 'none'       : Default if not defined.
-    -- 'all'        : If this is the first input to get this key sequence
-    --                then no other inputs listening for this sequence are fired.
-    -- 'script-only': If this is the first *custom* input to get this key sequence
-    --                then no other *custom* inputs listening for this sequence
-    --                are fired. Normal game inputs will still be fired even if
-    --                they match this sequence.
     -- 'game-only'  : The opposite of script-only. Blocks game inputs using the
     --                same key sequence but lets other custom inputs using the
     --                same key sequence fire.
